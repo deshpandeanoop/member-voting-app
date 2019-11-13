6145e9fa321246ab847b1c3238c40532cff89348
deshpandeanoop <anoop.bhardwaja@gmail.com>
2019-11-13 10:59:20 +0530

    Initial commit

---
+++ src/main/java/com/github/app/MemberVotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberVotingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberVotingAppApplication.class, args);
+	}
+
+}
